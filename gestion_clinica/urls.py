5a5fee94cd37734f7329074fbd472499a5dc4eb2
joaquin_cuadra <juakocuadra.m@gmail.com>
2025-09-15 03:46:03 -0300

    templates y urls corregidos

--- gestion_clinica/urls.py
+++ gestion_clinica/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,4 +8,5 @@ urlpatterns = [
     path("recepcion/", include("recepcion.urls")),
     path("diagnostico/", include("diagnostico.urls")),
     path("entrega/", include("entrega.urls")),
+    path("", lambda request: redirect("login/")),
 ]
