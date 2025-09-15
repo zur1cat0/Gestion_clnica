656b8b67a81b023d6666d058a395e2b6631c783f
joaquin_cuadra <juakocuadra.m@gmail.com>
2025-09-15 02:22:37 -0300

    app login agregada con clave predefinida y validacion

--- login/views.py
+++ login/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_view(request):
+    error = None
+    if request.method == "POST":
+        usuario = request.POST.get("usuario")
+        clave = request.POST.get("clave")
+        if usuario == "inacap" and clave == "clinica2025":
+            request.session["autenticado"] = True
+            return redirect("/recepcion/registrar/")
+        else:
+            error = "Usuario o clave incorrectos"
+    return render(request, "login/login.html", {"error": error})
